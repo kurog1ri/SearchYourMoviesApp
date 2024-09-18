d341e2088870cacb95654530037d7b568c302dac
kurog1ri <kosenko.yosiki@gmail.com>
2024-09-18 13:02:00 +0300

    Initial Commit

---
+++ SearchYourMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchYourMovies
+//
+//  Created by   Kosenko Mykola on 18.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchYourMovies/SearchYourMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchYourMoviesApp.swift
+//  SearchYourMovies
+//
+//  Created by   Kosenko Mykola on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchYourMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
